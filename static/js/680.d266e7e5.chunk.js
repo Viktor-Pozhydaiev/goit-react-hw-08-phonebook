decffc24c0fe85b5d4c0b0c8ad13ff236af82ec5
Viktor-Pozhydaiev <77990236+Viktor-Pozhydaiev@users.noreply.github.com>
2023-01-27 08:14:34 +0000

    Deploying to gh-pages from @ Viktor-Pozhydaiev/goit-react-hw-08-phonebook@1abeab314471a82897b5030dd5cc6496c11d7ec3 🚀

---
+++ static/js/680.d266e7e5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[680],{680:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var o=n(184);function c(){return(0,o.jsx)("div",{className:"flex justify-center items-center m-36 ",children:(0,o.jsx)("h2",{className:"text-center text-xl text-blue-700 font-bold",children:"Hello to contacts Books!"})})}}}]);
+//# sourceMappingURL=680.d266e7e5.chunk.js.map
